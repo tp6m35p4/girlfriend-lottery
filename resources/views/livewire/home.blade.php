441cb04a01400aec6cd9d34765ed8020659a5ef4
Yu-Cheng Chen <tp6m35p4@gmail.com>
2022-03-06 00:28:36 +0800

    used bg to green

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -6,13 +6,13 @@
         @foreach($lotteries as $lottery)
             <div class="flex content-center justify-center items-center border-4 border-pink-900 bg-pink-800 aspect-square">
                 @if($lottery->status)
-                <button class="flex flex-col justify-center align-middle items-center w-full h-auto bg-yellow-400 text-sm aspect-square" wire:click="toDetail('{{ $lottery->code }}')">
+                <button class="flex flex-col justify-center align-middle items-center w-full h-auto @if($lottery->is_used) bg-green-400 @else bg-yellow-400 @endif text-sm aspect-square" wire:click="toDetail('{{ $lottery->code }}')">
                     <p>{{ $lottery->title }}</p>
-                    @if($lottery->is_used)
-                        <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                        </svg>
-                        @endif
+{{--                    @if($lottery->is_used)--}}
+{{--                        <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">--}}
+{{--                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />--}}
+{{--                        </svg>--}}
+{{--                        @endif--}}
                 </button>
                 @else
                     <p class="flex justify-center items-center text-white" wire:click="">
